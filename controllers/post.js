7faadd34c4f1fabe9f6e6445b0192a7fa812af18
WebGCDev <gaetano.chi@gmail.com>
2024-06-06 17:44:09 +0200

    Add: BONUS 1 Implementare la paginazione.

--- controllers/post.js
+++ controllers/post.js
@@ -5,7 +5,7 @@ exports.createPost = async (req, res) => {
   const { title, content, slug, image, published, categoryId, tags } = req.body;
   try {
     const newPost = await prisma.post.create({
-      //Crea un nuovo post nel database utilizzando Prisma
+      // Crea un nuovo post nel database utilizzando Prisma
       data: {
         title,
         content,
@@ -16,7 +16,7 @@ exports.createPost = async (req, res) => {
         tags: { connect: tags.map((tagId) => ({ id: tagId })) },
       },
     });
-    res.status(201).json(newPost); //stato 201 (Created) e restituisce il nuovo post creato
+    res.status(201).json(newPost); // Stato 201 (Created) e restituisce il nuovo post creato
   } catch (error) {
     res
       .status(500)
@@ -48,28 +48,40 @@ exports.getPostBySlug = async (req, res) => {
 };
 
 exports.getPosts = async (req, res) => {
-  const { published, keyword } = req.query;
+  const { published, keyword, page = 1, pageSize = 10 } = req.query;
+  const skip = (page - 1) * pageSize;
 
   try {
-    let where = { published: true }; //recupera solo i post pubblicati
+    let where = { published: true }; // Recupera solo i post pubblicati
     if (keyword) {
-      // se presente parola chiave nalla ricerca, aggiunge condizione OR per cercare la parola chiave nel titolo o nel contenuto del post.
+      // Se presente una parola chiave nella ricerca, aggiunge condizione OR per cercare la parola chiave nel titolo o nel contenuto del post.
       where.OR = [
         { title: { contains: keyword, insensitive: true } },
         { content: { contains: keyword, insensitive: true } },
       ];
     }
 
+    const totalCount = await prisma.post.count({ where });
+    const totalPages = Math.ceil(totalCount / pageSize);
+
     const posts = await prisma.post.findMany({
-      //recupera post nel db e include le categorie e i tag associati a ciascun post
+      // Recupera post nel database e include le categorie e i tag associati a ciascun post
       where,
       include: {
         category: true,
         tags: true,
       },
+      skip,
+      take: parseInt(pageSize),
     });
 
-    res.status(200).json(posts); //stato 200 (OK) e restituisce tutti i post recuperati.
+    res.status(200).json({
+      totalCount,
+      totalPages,
+      currentPage: parseInt(page),
+      pageSize: parseInt(pageSize),
+      posts,
+    }); // Stato 200 (OK) e restituisce tutti i post recuperati con le informazioni di paginazione
   } catch (error) {
     res
       .status(500)
@@ -82,19 +94,17 @@ exports.updatePost = async (req, res) => {
   const { title, content, published, categoryId, tags } = req.body;
   try {
     const updatedPost = await prisma.post.update({
-      //aggiorna un post nel database
+      // Aggiorna un post nel database
       where: { slug },
       data: {
         title,
         content,
         published,
         categoryId,
-        tags: {
-          set: tags.map((tagId) => ({ id: tagId })),
-        },
+        tags: { set: tags.map((tagId) => ({ id: tagId })) },
       },
     });
-    res.status(200).json(updatedPost); //stato 200 (OK) e restituisce il post aggiornato
+    res.status(200).json(updatedPost); // Stato 200 (OK) e restituisce il post aggiornato
   } catch (error) {
     res.status(500).json({
       error: 'Impossibile aggiornare il post',
@@ -107,10 +117,10 @@ exports.deletePost = async (req, res) => {
   const { slug } = req.params;
   try {
     await prisma.post.delete({
-      //Elimina un post dal database
+      // Elimina un post dal database
       where: { slug },
     });
-    res.status(204).send(); //stato 204 (No Content) per indicare che il post è stato eliminato con successo
+    res.status(204).send(); // Stato 204 (No Content) per indicare che il post è stato eliminato con successo
   } catch (error) {
     res
       .status(500)
--- routes/post.js
+++ routes/post.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/post');
 
+router.get('/posts', postController.getPosts);
 router.post('/posts', postController.createPost);
 router.get('/posts/:slug', postController.getPostBySlug);
-router.get('/posts', postController.getPosts);
 router.put('/posts/:slug', postController.updatePost);
 router.delete('/posts/:slug', postController.deletePost);
 
